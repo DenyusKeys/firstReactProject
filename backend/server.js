3bf8358afc1056172864a5907708e52bbd049749
DeNyus Keys <denyusmt@yahoo.com>
2025-09-30 17:31:12 -0700

    Dynamic id grabbing using :id on endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,21 @@ const app = express();
 //API Route GET Request
 //(localhost:2222/api/notes)
 app.get("/api/notes", (req,res) => {
-    res.status(200).send("You got 5 notes");
+    res.status(200).send("You got 22 notes");
+})
+
+
+app.post("/api/notes", (req,res) => {
+    res.status(201).json({message:"Post CREATED successfully!"})
+})
+
+//Want to know which note it is: Need the id of the note (:id);
+app.put("/api/notes/:id", (req,res) => {
+    res.status(200).json({message:"Post UPDATED successfully!"})
+})
+
+app.delete("/api/notes/:id", (req,res) => {
+    res.status(200).json({message:"Post DELETED successfully!"})
 })
 
 app.listen(2222, () => {
